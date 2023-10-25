b1589a573b72578e8722cb291b4c7090a3775c43
OmarFaris7 <77802504+OmarFaris7@users.noreply.github.com>
2023-10-25 17:01:01 -0500

    Update terraform_code.tf

--- terraform_code.tf
+++ terraform_code.tf
@@ -1,1 +1,6 @@
-resource "aws_vpc" "main" {   cidr_block       = "10.0.0.0/16"   instance_tenancy = "default"   tags = {     Name = "main"   } }
+resource "aws_vpc" "main" {
+  cidr_block = "10.0.0.0/16"
+    tags = {
+    Name = "main"
+  }
+}
